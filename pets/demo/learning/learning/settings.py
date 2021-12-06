0776e0061b57aed12a16e6446745e8907389805e
Pavel <p.a.kons@yandex.ru>
2021-12-06 16:56:15 +0300

    Deploy to Heroku

--- pets/demo/learning/learning/settings.py
+++ pets/demo/learning/learning/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+django_heroku.settings(locals())
