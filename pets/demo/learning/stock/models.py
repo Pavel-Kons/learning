668fa43edf4f54c8c9a97699fa9038be5fc9c70b
Pavel <p.a.kons@yandex.ru>
2021-12-24 15:35:17 +0300

    Description added to stock

--- pets/demo/learning/stock/models.py
+++ pets/demo/learning/stock/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Stock(models.Model):
 	name = models.CharField(max_length=40)
 	ticker = models.CharField(max_length=4, default="NULL")
+    description = models.TextField(null=True, blank=True)
 
 
 class Currency(models.Model):
