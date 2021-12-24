e8ac1d1e9c2428cc28e7409331bc3b679ed3732f
Pavel <p.a.kons@yandex.ru>
2021-12-24 15:46:16 +0300

    Deleted \

--- pets/demo/learning/stock/models.py
+++ pets/demo/learning/stock/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Stock(models.Model):
     name = models.CharField(max_length=40)
-    ticker = models.CharField(max_length=4, default="NULL")\
+    ticker = models.CharField(max_length=4, default="NULL")
     description = models.TextField(null=True, blank=True)
 
 
